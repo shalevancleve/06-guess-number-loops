f6114b78a78fd16c3c62a0269f3b1bbb266d8dd3
Guil H <g.hernandez07@gmail.com>
2025-02-26 21:36:05 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-// Generate a random number between 1 and 10
-const secretNumber = Math.floor(Math.random() * 10) + 1;
+document.getElementById('start-button').addEventListener('click', function() {
+  // Generate a random number between 1 and 10
+  const secretNumber = Math.floor(Math.random() * 10) + 1;
+
+});
